07e15e939bbeccbf607a715ed5a5cb83b9bb4a7a
Alexander Rathour <RathouA@StPaulsSchool.org.uk>
2022-09-16 00:06:19 +0100

    added exercises and program.cs

--- CSharpConcepts/Exercises.cs
+++ CSharpConcepts/Exercises.cs
@@ -10,24 +10,56 @@ namespace CSharpConcepts
 {
     internal class Exercises
     {
-        internal static void Run()
+        internal static void Run(string rpsUserChoice, double lg1, double lg2, double lg3)
         {
-            CallTypeExercises();
-            CallSelectionExercises();
+            //CallTypeExercises();
+            CallSelectionExercises(rpsUserChoice, lg1, lg2, lg3);
         }
 
         private static void CallTypeExercises()
         {
             // Add your calling code here for the questions an example is below
 
-            //Console.WriteLine("# Calculator");
-            //TypeExercises.Calculator(12, 5);
-            //Console.WriteLine();
+            Console.WriteLine("# Calculator");
+            TypeExercises.Calculator("12", "5");
+
+            Console.WriteLine("\n# Binary Strings");
+            TypeExercises.BinaryStrings(10);
+
+            Console.WriteLine("\n# BMI");
+            Console.WriteLine(TypeExercises.BMICalculator(1.75,65));
+
+            Console.WriteLine("\n# Future Date");
+            Console.WriteLine(TypeExercises.FutureDate(2));
+
+            Console.WriteLine("\n# Select Language");
+            Console.WriteLine(TypeExercises.SelectLanguage("chinese"));
+
         }
 
-        private static void CallSelectionExercises()
+        private static void CallSelectionExercises(string rpsUserChoice, double lg1, double lg2, double lg3)
         {
             // Add your calling code here for the selection questions
+
+            /*Console.WriteLine("# Fizz Buzz");
+            Console.WriteLine(SelectionExercises.FizzBuzz(15));
+
+            Console.WriteLine("\n# Vowel Or Consonant");
+            Console.WriteLine(SelectionExercises.VowelOrConsonant('e'));
+
+            Console.WriteLine("\n# Dice Roller");
+            Console.WriteLine(SelectionExercises.DiceRoller());
+
+            Console.WriteLine("\n# Angles");
+            Console.WriteLine(SelectionExercises.Angles(270));
+
+            Console.WriteLine("\n# Rock Paper Scissors");
+            SelectionExercises.RockPaperScissors(rpsUserChoice); //need to uncomment user input in program.cs to work*/
+
+            Console.WriteLine("\n# Triangle");
+            Console.WriteLine(SelectionExercises.Triangle(lg1,lg2,lg3)); //need to uncomment user input in program.cs to work
+
+
         }
     }
 }
--- CSharpConcepts/Program.cs
+++ CSharpConcepts/Program.cs
@@ -5,10 +5,20 @@
         static void Main()
         {
             // Calls the example code
-            Examples.Run();
+            //Examples.Run();
 
             // Calls the exercises code
-            Exercises.Run();
+
+            string userRSP = "";
+            //Console.WriteLine("What is your choice for rock paper scissors?"); //how to call this correctly when other exercises running too?
+            //string userRSP = Console.ReadLine();
+
+            Console.WriteLine("Enter 3 lengths of the triangle:");
+            double.TryParse(Console.ReadLine(), out double side1);
+            double.TryParse(Console.ReadLine(), out double side2);
+            double.TryParse(Console.ReadLine(), out double side3);
+
+            Exercises.Run(userRSP, side1, side2, side3);
         }
 
        

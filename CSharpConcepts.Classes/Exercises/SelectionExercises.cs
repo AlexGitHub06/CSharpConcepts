feaeee20cb5498e4ac06e14d1ffeed7052a3f857
Alexander Rathour <RathouA@StPaulsSchool.org.uk>
2022-09-16 00:02:51 +0100

    finished exercises

--- CSharpConcepts.Classes/Exercises/SelectionExercises.cs
+++ CSharpConcepts.Classes/Exercises/SelectionExercises.cs
@@ -13,7 +13,14 @@ namespace CSharpConcepts.Classes.Exercises
         // If divisible by neither return the number itself (as a string)
         public static string FizzBuzz(int num)
         {
-            throw new NotImplementedException();
+            string result;
+
+            if (num % 3 == 0 && num % 5 == 0) { result = "FizzBuzz"; }
+            else if (num % 3 == 0) { result = "Fizz"; }
+            else if (num % 5 == 0) { result = "Buzz"; }
+            else { result = num.ToString(); }
+
+            return result;
         }
 
         // Q2: Vowel or Consonant
@@ -21,29 +28,146 @@ namespace CSharpConcepts.Classes.Exercises
         // false if it is a consonant
         public static bool VowelOrConsonant(char letter)
         {
-            throw new NotImplementedException();
+            bool isVowel;
+
+            switch (letter)
+            {
+                case 'a':
+                    isVowel = true;
+                    break;
+                case 'e':
+                    isVowel = true;
+                    break;
+                case 'i':
+                    isVowel= true;
+                    break;
+                case 'o':
+                    isVowel = true;
+                    break;
+                case 'u':
+                    isVowel = true;
+                    break;
+                default:
+                    isVowel = false;
+                    break;  
+            }
+
+            return isVowel;
         }
 
         // Q3: DiceRoller
         // Write a procedure which rolls three dice and write the following messages to the console
         // If all three dice have the same value write, "Three of a kind"
         // If two of the dice have the same value write, "You have a pair!
-        // Otherwise write, "Sorry you lose
+        // Otherwise write, "Sorry you lose"
+
+        public static string DiceRoller()
+        {
+            string output;
+
+            Random random = new Random();
+
+            int dice1 = random.Next(0, 7);
+            int dice2 = random.Next(0, 7);
+            int dice3 = random.Next(0, 7);
+
+            Console.WriteLine($"{dice1},{dice2},{dice3}");
+
+            if (dice1 == dice2 && dice2 == dice3) { output = "Three of a kind"; }
+            else if (dice1 == dice2 | dice1 == dice3 | dice2 == dice3) { output = "You have a pair!"; }
+            else { output = "Sorry you lose"; }
+
+            return output;
+
+        }
 
         // Q4: Angles
         // Write a function to return whether the angle input is acute, right angle, obtuse, straight or reflex
         // Use an enum for the different angle types
+
+        public enum Angle
+        {
+            Acute,
+            RightAngle,
+            Obtuse,
+            Straight,
+            Reflex
+        }
+
+        public static string Angles(double angle)
+        {
+            string angleType = "";
+
+            switch (angle)
+            {
+                case > 180:
+                    angleType = Angle.Reflex.ToString();
+                    break;
+                case 180:
+                    angleType = Angle.Straight.ToString();
+                    break;
+                case > 90:
+                    angleType = Angle.Obtuse.ToString();
+                    break;
+                case 90:
+                    angleType = Angle.RightAngle.ToString();
+                    break;
+                case < 90:
+                    angleType = Angle.Acute.ToString();
+                    break;
+                default:
+                    angleType = "invalid input";
+                    break;
+            }
+
+            return angleType;
+        }
 
         // Q5: Rock, Paper Scissors
         // Write a procedure which takes the user's choice as input and randomly generate the computer's choice
         // It should write the computer's choice to the console and write whether the user lost, won, or drew.
         // Handle the user input in the calling code in Program.cs
 
+        public enum RPSChoices
+        {
+            rock,
+            paper,
+            scissors
+        }
+
+        public static void RockPaperScissors(string userChoice)
+        {
+
+            Random random = new Random();
+            int num = random.Next(0, 3);
+            string computerChoice = ((RPSChoices)num).ToString();
+
+            Console.WriteLine($"User choice: {userChoice}");
+            Console.WriteLine($"Computer choice: {computerChoice}");
+
+            int userNum = (int)Enum.Parse(typeof(RPSChoices), userChoice.ToLower());
+
+            if (userNum - num == 1 || userNum - num == -2) { Console.WriteLine("User won"); }
+            else if (num - userNum == 1 || num - userNum == -2) { Console.WriteLine("User lost"); }
+            else if (num - userNum == 0) { Console.WriteLine("Draw"); }
+
+        }
+
         // Q6: Triangle
         // Write a function that takes in three side lengths.
         // Determine whether a triangle with those sides is equilateral, isosceles, scalene or impossible
 
+        public static string Triangle(double side1, double side2, double side3)
+        {
+            string shape;
 
+            if (side1 + side2 < side3 || side2 + side3 < side1 || side1 + side3 < side2) { shape = "impossible"; }
+            else if(side1 == side2 && side2 == side3) { shape = "equilateral"; }
+            else if (side1 == side2 | side2 == side3 | side1 == side3) { shape = "isosceles"; }
+            else { shape = "scalene"; }
+
+            return shape;
+        }
 
     }
 }
--- CSharpConcepts.Classes/Exercises/TypeExercises.cs
+++ CSharpConcepts.Classes/Exercises/TypeExercises.cs
@@ -12,35 +12,55 @@ namespace CSharpConcepts.Classes.Exercises
         // Write to the console correctly the results of: +. - , *, /
         public static void Calculator(int num1, int num2)
         {
-            throw new NotImplementedException();    
+            Console.WriteLine(num1 + num2);
+            Console.WriteLine(num1 - num2);
+            Console.WriteLine(num1 * num2);
+            Console.WriteLine((double)num1 / (double)num2);
+            
         }
 
         // Q2: Calculator 2
         // Try to convert both the values to a double and write to the console the results of: +. - , *, /
         public static void Calculator(string value1, string value2)
         {
-            throw new NotImplementedException();
+
+            double.TryParse(value1, out double doubleV1);
+            double.TryParse(value2, out double doubleV2);
+
+            Console.WriteLine(doubleV1 + doubleV2);
+            Console.WriteLine(doubleV1 - doubleV2);
+            Console.WriteLine(doubleV1 * doubleV2);
+            Console.WriteLine(doubleV1 / doubleV2);
         }
 
         // Q3: Binary Strings
         // Write to the console the number printed in Binary, Decimal and Hex formats
         public static void BinaryStrings(int num)
         {
-            throw new NotImplementedException();
+            string binaryV = Convert.ToString(num, 2);
+            string decimalV = ((decimal)num).ToString();
+            string hexV = Convert.ToString(num,16);
+
+            Console.WriteLine($"{num}, {binaryV} in binary, {decimalV} in decimal, {hexV} in hex");
         }
 
         // Q4: BMI
         // Calculate and return the BMI - Body mass index
         public static double BMICalculator(double height, double weight)
         {
-            throw new NotImplementedException();
+            double bmi = weight / (height * height);
+
+            return bmi;
         }
 
         // Q5: FutureDate
         // Write to the console today's date. Return the date that it will be when the days passed in are added to the current date. 
         public static DateTime FutureDate(int days)
         {
-            throw new NotImplementedException();
+            DateTime today = DateTime.Today;
+            DateTime twoDays = today.AddDays(days);
+
+            return today;
         }
         
         // Q6: Languages
@@ -55,7 +75,22 @@ namespace CSharpConcepts.Classes.Exercises
         }
         public static Language SelectLanguage(string proposedLanguage)
         {
-            throw new NotImplementedException();
+            if (Enum.TryParse(proposedLanguage, true, out Language language))
+            {
+                return language;
+            }
+
+            else
+            {
+                Console.WriteLine("Sorry your language was not valid. You will be randomly assigned a new language");
+
+                Random random = new Random();
+                int choice = random.Next(0,4);
+                language = (Language)choice;
+
+                return language;
+
+            }
         }
 
 
